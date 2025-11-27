45eb5aa9bd5bd088787c92d91b8d3eea8e2e8408
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-11-27 13:56:59 +0300

    removed sdk from the front end

--- frontend/lib/santimpay/utils/cryptography.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-export function sign(payload, privateKey, algorithm) {
-  return jwt.sign(JSON.stringify(payload), privateKey, {
-    algorithm,
-  });
-}
-export function signES256(payload, privateKey) {
-  return sign(payload, privateKey, "ES256");
-}
