33ba5dda0bfbe76174f8350bb99e91acba1d1261
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-11-29 00:59:39 +0300

    fix: standardize formatting in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'pazimo.com'],
+    domains: ["localhost", "pazimo.com"],
   },
 
   eslint: {
@@ -17,13 +17,17 @@ const nextConfig = {
 
   // Set the production API URL
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === 'production' 
-      ? 'https://pazimoapp.testserveret.com' 
-      : 'http://localhost:5000'),
-    NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL || (process.env.NODE_ENV === 'production'
-      ? 'https://pazimo.vercel.app'
-      : 'http://localhost:3000'),
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL ||
+      (process.env.NODE_ENV === "production"
+        ? "https://pazimoapp.testserveret.com"
+        : "http://localhost:5000"),
+    NEXT_PUBLIC_FRONTEND_URL:
+      process.env.NEXT_PUBLIC_FRONTEND_URL ||
+      (process.env.NODE_ENV === "production"
+        ? "https://pazimo.vercel.app"
+        : "http://localhost:3000"),
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
