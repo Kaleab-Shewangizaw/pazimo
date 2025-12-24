0233106231e73b3020db0529d146be76cf68587a
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-12-24 22:12:46 +0300

    zoho returned

--- backend/src/controllers/invitationEmailController.js
+++ backend/src/controllers/invitationEmailController.js
@@ -7,10 +7,12 @@ const createTransporter = () => {
   }
 
   return nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.zoho.com",
+    port: 587,
+    secure: false, // Use TLS
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER_ZOHO,
+      pass: process.env.EMAIL_PASS_ZOHO,
     },
   });
 };
