aa9b99a56c54e0c676e5f1463dd90d92ebb4605c
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-12-29 11:51:35 +0300

    loading spinner changed

--- frontend/app/organizer/page.tsx
+++ frontend/app/organizer/page.tsx
@@ -515,9 +515,8 @@ export default function OrganizerDashboard() {
     return (
       <div className="flex items-center justify-center min-h-screen">
         <div className="text-center">
-          <Loader2 className="h-12 w-12 animate-spin text-blue-600 mx-auto mb-4" />
-          <h2 className="text-2xl font-semibold mb-2">Loading Dashboard...</h2>
-          <p className="text-gray-500">Please wait while we fetch your data</p>
+          {/* spinner div */}
+          <div className="border-t-2 rounded-full p-10 spinner border-blue-700" />
         </div>
       </div>
     );
